e6d60886571aa3ead9297da08e97721f0710f947
daniel contreras <daniel@sciensomnia.com>
2018-04-18 00:41:09 -0500

    Added full Yarn Functionality to pull yarn movie quotes in gif format

--- commands/yarn.js
+++ commands/yarn.js
@@ -1,3 +1,8 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+var results = [];
+
 module.exports = {
 	command: 'yarn',
 	aliases: [],
@@ -5,9 +10,28 @@ module.exports = {
 	description: 'Get a short gif of a movie quote!',
 	usage: 'yarn <Quote to search for>',
 	execute: (bot, user, userID, channelID, args, event) => {
-        bot.sendMessage({
-            to:channelID,
-            message: 'Still in testing!'
-        })
+		const options = {
+			uri: `https://getyarn.io/yarn-find?text=${escape(args.join(' '))}`,
+			transform: function (body) {
+				return cheerio.load(body);
+			}
+		};
+		rp(options)
+			.then(($) => {
+				$('a').each(function (i, elem) {
+					if ($(elem)['0'].attribs.href.startsWith('/yarn-clip/')) {
+						results.push($(elem)['0'].attribs.href.substr(11, $(elem)['0'].attribs.href.length))
+					}
+				});
+				bot.sendMessage({
+					to:channelID,
+					message: 'https://y.yarn.co/'+results[Math.floor(Math.random() * results.length)] + '_text_hi.gif'
+				}, ()=>{
+					results = [];
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
 };
