acacce5a2a86a9f0205cec1927fc651789344a4f
daniel contreras <daniel@sciensomnia.com>
2018-05-21 13:20:13 -0500

    Added Black and White photo manipulator. Added new cactus gif. Added new ability to select which gif to show if a number in range is used as an argument

---
+++ commands/black-and-white.js
@@ -0,0 +1,58 @@
+var jimp = require('jimp');
+var fs = require('fs')
+
+var white = 4294967295;
+var black = 0;
+
+module.exports = {
+    command: 'black-and-white',
+    aliases: ['bw'],
+    category: 'Image Manipulation',
+    description: 'Returns a photo in black and white. Note: This is not grayscale',
+    usage: 'black-and-white <image URL>',
+    execute: (bot, user, userID, channelID, args, event) => {
+        if (args[0] == undefined || !ValidURL(args[0])) {
+            bot.sendMessage({
+                to: channelID,
+                message: 'Invalid URL, please provide a valid URL to an image'
+            })
+            return
+        }
+        jimp.read(args[0], (err, image) => {
+            if (err || image == undefined) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Invalid URL, please provide a valid URL to an image'
+                })
+                return
+            }
+
+            for (var i = 0; i < image.bitmap.height; i++) { // for each row in image
+                for (var n = 0; n < image.bitmap.width; n++) { // for each column in image
+                    var color = jimp.intToRGBA(image.getPixelColor(n, i))
+                    var luma = 0.2989 * color.r + 0.5870 * color.g + 0.1140 * color.b;
+                    if(color.a < 255) console.log('woah')
+                    if (luma > 105) { //convert to white
+                        image.setPixelColor(white, n, i)
+                    } else { //convert to black
+                        image.setPixelColor(black, n, i)
+                    }
+                }
+            }
+
+            image.write('./temp.jpg', () => {
+                bot.uploadFile({
+                    to: channelID,
+                    file: './temp.jpg'
+                }, () => {
+                    fs.unlink('./temp.jpg', () => {})
+                })
+            })
+        })
+    }
+};
+
+function ValidURL(str) {
+    var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return pattern.test(str);
+}
--- commands/cactus.js
+++ commands/cactus.js
@@ -2,7 +2,8 @@ var cacti = [
     'https://media.giphy.com/media/3oEduItbzfD1Xmqhwc/giphy.gif',
     'https://media.giphy.com/media/l0HlHgiDgsW2c32p2/giphy.gif',
     'https://media.giphy.com/media/wS8OH0T4GKHIY/giphy.gif',
-    'https://media.giphy.com/media/l0IyebXezC6jXbyoM/giphy.gif'
+	'https://media.giphy.com/media/l0IyebXezC6jXbyoM/giphy.gif',
+	'https://cdn.discordapp.com/attachments/439081106382520341/448178321591762944/tumblr_o8ofmbbZx41ufu9cno1_1280.gif'
 ]
 
 var embed = require('../styling/gif_embed.json');
@@ -14,15 +15,35 @@ module.exports = {
 	description: 'Surprise! Cactus!',
 	usage: 'cactus',
 	execute: (bot, user, userID, channelID, args, event) => {
-		bot.sendMessage({
-			to: channelID,
-			embed:{
-				title: 'cactus',
-				color: embed.color,
-				image:{
-					url: cacti[Math.floor(Math.random() * cacti.length)]
+		if(args[0] == undefined || args[0] == NaN){
+			bot.sendMessage({
+				to: channelID,
+				embed:{
+					title: 'cactus',
+					color: embed.color,
+					image:{
+						url: cacti[Math.floor(Math.random() * cacti.length)]
+					}
 				}
-			}
-		})
+			})
+		}
+		else if(args[0] > cacti.length){
+			bot.sendMessage({
+				to:channelID,
+				message: 'Please select a gif from the range 1 - '+cacti.length
+			})
+		}
+		else{
+			bot.sendMessage({
+				to: channelID,
+				embed:{
+					title: 'cactus',
+					color: embed.color,
+					image:{
+						url: cacti[args[0]-1]
+					}
+				}
+			})
+		}
 	}
 };
